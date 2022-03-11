a317fb27d96224d4918f9f1760621dd8d477e45e
hisham-css <61122654+hisham-CSS@users.noreply.github.com>
2022-03-11 15:08:32 -0500

    Hisham adding React-Router-Dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { HashRouter, Route, Link } from "react-router-dom";
+import Home from "./home.js";
+import StartPage from "./startpage.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Foody App 
-        </p>
-        <a
+    <HashRouter basename="/">
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Foody App</p>
+          {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Start
-        </a>
-      </header>
-    </div>
+        > */}
+
+          <Link to="/start">Start</Link>
+          <Link to="/">Home</Link>
+        </header>
+
+        <Route exact path="/" component={Home} />
+        <Route path="/start" component={StartPage} />
+      </div>
+    </HashRouter>
   );
 }
 
