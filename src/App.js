8e3fd64d7ddeef16a2f321836766aebd60288350
Ava <zahra.arjmandd@gmail.com>
2022-03-22 15:05:22 -0400

    fixinherrors

--- src/App.js
+++ src/App.js
@@ -4,20 +4,34 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Meals from "./Component/Meals";
 import StartPage from "./Component/Startpage";
 import Header from "./Component/Header";
-import askCompletion from "./Component/askCompletion/askCompletion";
+import AskCompletion from "./Component/AskCompletion";
+import Recipe from "./Component/Recipe";
+import SesameNoosdleBowl from "./Component/SesameNoodleBowl";
+
+
+
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route
-          exact
-          path={process.env.PUBLIC_URL + "/"}
-          component={<StartPage />}
-        />
+
+
+      <Route>
+        <Route exact path={process.env.PUBLIC_URL + "/"} component={StartPage} />
         <Route path={process.env.PUBLIC_URL + "/Meals"} component={<Meals />} />
+        <Route path={process.env.PUBLIC_URL + "/Recipe"} component={<Recipe />} />
+        <Route path={process.env.PUBLIC_URL + "/quickAndEasy"} component={<quickAndEasy/>} />
+        <Route path={process.env.PUBLIC_URL + "/style_diet"} component={<style_diet/>} />
+        <Route path={process.env.PUBLIC_URL + "/AskCompletion"} component={<AskCompletion/>} />
+        <Route path={process.env.PUBLIC_URL + "/SesameNoosdleBowl"} component={<SesameNoosdleBowl/>} />
+      </Route>
+      
       </Routes>
+
+
+
     </Router>
   );
 }
--- src/Component/recipe/index.js
+++ src/Component/recipe/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import{Navbar,Nav,NavDropdown,Form,FormControl,Button} from 'react-bootstrap'
+import{Navbar,Nav,NavDropdown,Form,FormControl,Button,Container} from 'react-bootstrap'
 export default class Navbarcomp extends Component {
     render(){
          return(
