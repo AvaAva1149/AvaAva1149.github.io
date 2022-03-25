5fad5b03848c8d95d7c0a519b13f1732e39d3a41
Saori-design <65802428+Saori-design@users.noreply.github.com>
2022-03-25 01:33:30 -0400

    Added page 10, and updated page 9 by saori

---
+++ src/Component/StartCooking/index.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>
+         Let's Cook With FOODY
+        </h2>
+
+        <div className="Ingredients">
+        <h4>INGREDIENTS</h4>
+          <h6>You cna use your voice to play the cooking video</h6>
+          <h2>Put the phone on the table and speak up</h2>
+        </div>
+
+        <button cLassName="video-button">Start Cooking</button>
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
--- src/Component/sesamenoodlebowl/index.js
+++ src/Component/sesamenoodlebowl/index.js
@@ -3,15 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import logo from './logo.svg';
+import './App.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>
+         SESAME NOODLE BOWL
+        </h2>
+        <img src="https://images.unsplash.com/photo-1617093727343-374698b1b08d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8bm9vZGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"></img>
+        <div className="Ingredients">
+        <h4>INGREDIENTS</h4>
+          <p>Noodle 1 pack</p>
+          <p>Garlic 1 piece</p>
+          <p>Cucumber x 1</p>
+          <p>Tahini x 1tbsp</p>
+          <p>Sesame paste 6g</p>
+          <p>Soy sauce 3 tbsp</p>
+          <p>Vinegar 1 tbsp</p>
+        </div>
+        <button cLassName="Video-button">Watch video</button>
+      </header>
+      
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default App;
