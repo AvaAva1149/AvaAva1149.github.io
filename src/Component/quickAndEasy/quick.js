a12915634a87b65f06903c77762401d8221b108d
jumarash <jumarash29@gmail.com>
2022-03-25 14:18:18 -0400

    quick

---
+++ src/Component/quickAndEasy/quick.js
@@ -0,0 +1,21 @@
+import "../../App.css";
+import React from "react";
+
+function QuickAndEasy() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Testing</h2>
+
+        <div className="Ingredients">
+          <h4>Recipe</h4>
+          
+        </div>
+
+        <button cLassName="video-button">Hello</button>
+      </header>
+    </div>
+  );
+}
+
+export default QuickAndEasy;
