725b8ae8d1ca4751445c2f9bba7c9dc68c077ab1
Saori-design <65802428+Saori-design@users.noreply.github.com>
2022-03-31 23:13:09 -0400

    updated
    updated

--- src/Component/StartCooking/index.js
+++ src/Component/StartCooking/index.js
@@ -14,8 +14,13 @@ function StartCooking() {
           <h6>You cna use your voice to play the cooking video</h6>
           <h2>Put the phone on the table and speak up</h2>
         </div>
-
-        <button cLassName="start-video-button">Start Cooking</button>
+        
+        <img src="https://images.unsplash.com/photo-1617093727343-374698b1b08d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8bm9vZGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"></img>
+       
+        <Link to="Component/Video">
+          <button cLassName="start-video-button">Start Cooking</button>
+        </Link>
+       
       </header>
     </div>
   );
