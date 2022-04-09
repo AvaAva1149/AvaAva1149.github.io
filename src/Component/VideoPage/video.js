47c496870c7e19b6e8d85f100b71dc00d8c0e65f
Saori-design <65802428+Saori-design@users.noreply.github.com>
2022-04-08 22:27:45 -0400

    Update

--- src/Component/VideoPage/video.js
+++ src/Component/VideoPage/video.js
@@ -1,48 +1,93 @@
 import mic from "../../mic.svg";
 import "../../App.css";
-import ReactPlayer from "react-player";
+// import ReactPlayer from "react-player";
+
+//Saori test
+import VoiceModel from "../voiceCommandData/voice_model/model.json";
+import VoiceModelMetadata from "../voiceCommandData/metadata.json";
 
 import { Link } from "react-router-dom";
 
 import "../../index.css";
 import "./style.css";
+import { FaDrawPolygon } from "react-icons/fa";
+
+let video;
+
+let classifier;
 
 
-function VideoPage() {
-  return (
-    <div className="App-video">
+//setup
+// function VideoPage() {
+//   return (
+//     <div className="App-video">
 
 
-      <header className="App-header-video">
+//Test-1 use function
+
+function preload(){
+  classifier = m15.voiceClassifier('https://teachablemachine.withgoogle.com/models/WdxrUTqzf/')
+}
+
+//playing video with voice
+function classifyVoice(){
+  classifier.classify (video, controlVideo);
+
+}
+
+//get Result
+function controlVideo(){
+  if (play == '0.7') {
+    play.video;
+  } else if(pause == '0.7'){
+    pause.video;
+  } else if(forward == '0.7'){
+    pause.video;
+  }
+
+}
+  classify();
+
+  //Test-3
+
+  startVideo.
+
+
+
+//       <header className="App-header-video">
        
-        <p> SPEAK UP TO CONTROL THE VIDEO</p>
+//         <p> SPEAK UP TO CONTROL THE VIDEO</p>
 
-        <img src={mic} className="mic" alt="mic" />
+//         <img src={mic} className="mic" alt="mic" />
 
-        <div className="vidyoutu" >
+//         <div className="vidyoutu" >
 
-        <ReactPlayer url="youtu.be/vPFoCNnaqIQ"/>
-        </div>
-
-       
-
-
-         <Link to="/askCompletion">
-
-         <button className="linkButton-video" type="button">
-       Completed!
-        </button>
+//         <ReactPlayer url="youtu.be/vPFoCNnaqIQ"/>
+//         </div>
 
        
-      </Link>
+
+
+//          <Link to="/askCompletion">
+
+//          <button className="linkButton-video" type="button">
+//        Completed!
+//         </button>
+
+       
+//       </Link>
       
      
       
       
-</header>
-    </div>
-  );
-}
+// </header>
+//     </div>
+//   );
+// }
 
 
-export default VideoPage;
+// export default VideoPage;
+
+
+
+
